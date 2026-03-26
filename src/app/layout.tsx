efec89e264252ec158f08b0dd6a0e4463140a0b5
Dimitris Tsironis <tsironis@admin.grnet.gr>
2026-03-26 15:41:18 +0200

    Add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={noto.className}>{children}</body>
       <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5NHQFR2NKS"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-5NHQFR2NKS');
+      `}</Script>
+      <Script
         id="beacon-1"
         type="text/javascript"
       >{`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});`}</Script>
