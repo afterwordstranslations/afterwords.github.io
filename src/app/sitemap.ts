f99519c8666686210cedd400663a1f87b50e7983
Dimitris Tsironis <tsironis@admin.grnet.gr>
2026-04-10 19:08:00 +0300

    Fix build for metadata pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,8 @@ import { getAllPosts } from "~/lib/blog";
 
 const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://afterwords.gr";
 
+export const dynamic = "force-static";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getAllPosts();
 
